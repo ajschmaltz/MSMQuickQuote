c790fee598b9f669601ad80d6eb9c5cb95e469f7
luscadigital <joey@luscadigital.com>
2015-02-02 13:50:05 -0500

    working on database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,210 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Mower Matcher</title>
+  <link href='http://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
-  <!-- Bootstrap -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      height: 100%;
+      color: #B0BEC5;
+      display: table;
+      font-weight: 100;
+      font-family: 'Lato';
+    }
 
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+    .container {
+      text-align: center;
+      display: table-cell;
+      vertical-align: middle;
+    }
+
+    .content {
+      text-align: center;
+      display: inline-block;
+    }
+
+    .title {
+      font-size: 96px;
+      margin-bottom: 40px;
+    }
+
+    .quote {
+      font-size: 24px;
+    }
+  </style>
 </head>
-<body ng-app="mm" style="padding: 5px;">
-<div ng-controller="matchCtrl">
-  <div ng-hide="printMode" class="row">
-    <div class="col-md-2">
-      <form class="form">
-        <div class="form-group form-group-sm">
-          <label>Presets</label>
-          <select class="form-control" ng-change="setPreset(preset)" ng-model="preset">
-            <option value="">Select</option>
-            <optgroup label="Homeowner">
-              <option>Walk Behind</option>
-              <option>Walk Behind - Eco</option>
-              <option>Walk Behind - Premium</option>
-              <option>Zero Turn</option>
-              <option>Zero Turn - Eco</option>
-              <option>Zero Turn - Premium</option>
-            </optgroup>
-            <optgroup label="Mid Duty">
-              <option>Mid Duty</option>
-              <option>Mid Duty - Eco</option>
-              <option>Mid Duty - Premium</option>
-            </optgroup>
-            <optgroup label="Commercial">
-              <option>Small Spaces</option>
-              <option>Rider - Eco</option>
-              <option>Rider - Premium</option>
-              <option>Stander</option>
-            </optgroup>
-          </select>
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Brand</label>
-          <select class="form-control" ng-model="search.Brand">
-            <option value="">Show All</option>
-            <option ng-repeat="mower in filteredMowers | unique: 'Brand'">[[ mower.Brand ]]</option>
-          </select>
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Series</label>
-          <select class="form-control" ng-model="search.Series">
-            <option value="">Show All</option>
-            <option ng-repeat="mower in filteredMowers | unique: 'Series'">[[ mower.Series ]]</option>
-          </select>
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Model</label>
-          <input ng-model="search.Model" class="form-control"/>
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Style</label>
-          <select class="form-control" ng-model="search.Style">
-            <option value="">Show All</option>
-            <option ng-repeat="mower in filteredMowers | unique: 'Style'">[[ mower.Style ]]</option>
-          </select>
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Use</label>
-          <select class="form-control" ng-model="search.Use">
-            <option value="">Show All</option>
-            <option ng-repeat="mower in filteredMowers |  unique: 'Use'">[[ mower.Use ]]</option>
-          </select>
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Width</label>
-          <input placeholder="Min Width" type="number" ng-model="Width.min" class="form-control" />
-          -
-          <input placeholder="Max Width" type="number" ng-model="Width.max" class="form-control" />
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Engine</label>
-          <select class="form-control" ng-model="search.Engine">
-            <option value="">Show All</option>
-            <option ng-repeat="mower in filteredMowers | unique: 'Engine'">[[ mower.Engine ]]</option>
-          </select>
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Engine Model</label>
-          <select class="form-control" ng-model="search.Engine_Model">
-            <option value="">Show All</option>
-            <option ng-repeat="mower in filteredMowers | unique: 'Engine_Model'">[[ mower.Engine_Model ]]</option>
-          </select>
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Fuel</label>
-          <select class="form-control" ng-model="search.Fuel">
-            <option value="">Show All</option>
-            <option ng-repeat="mower in filteredMowers | unique: 'Fuel'">[[ mower.Fuel ]]</option>
-          </select>
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Sale Price</label>
-          <input placeholder="Min Price" type="number" ng-model="Sale.min" class="form-control" />
-          -
-          <input placeholder="Max Price" type="number" ng-model="Sale.max" class="form-control" />
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Financing</label>
-          <input placeholder="Min Finance" type="number" ng-model="Finance.min" class="form-control" />
-          -
-          <input placeholder="Max Finance" type="number" ng-model="Finance.max" class="form-control" />
-        </div>
-        <div class="form-group form-group-sm">
-          <label>Warranty</label>
-          <select class="form-control" ng-model="search.Warranty">
-            <option value="">Show All</option>
-            <option ng-repeat="mower in filteredMowers | unique: 'Warranty'">[[ mower.Warranty ]]</option>
-          </select>
-        </div>
-      </form>
-    </div>
-    <div class="col-md-10">
-      <table class="table table-hover table-condensed small">
-        <thead>
-        <tr>
-          <th>Brand</th>
-          <th>Series</th>
-          <th>Model</th>
-          <th>Style</th>
-          <th>Use</th>
-          <th>Width</th>
-          <th>HP</th>
-          <th>Engine</th>
-          <th>Engine Model</th>
-          <th>Fuel</th>
-          <th>MSRP</th>
-          <th>Sale</th>
-          <th>Event</th>
-          <th>Finance</th>
-          <th>Warranty</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr ng-repeat="mower in mowers | filter:search | rangeFilter:{min: Width.min, max: Width.max, property: 'Width'}  | rangeFilter:{min: Sale.min, max: Sale.max, property: 'Sale'} | rangeFilter:{min: Finance.min, max: Finance.max, property: 'Finance'} | orderBy : 'MSRP' | as:this:'filteredMowers'">
-          <td>[[ mower.Brand ]]</td>
-          <td>[[ mower.Series ]]</td>
-          <td>[[ mower.Model ]]</td>
-          <td>[[ mower.Style ]]</td>
-          <td>[[ mower.Use ]]</td>
-          <td>[[ mower.Width ]]</td>
-          <td>[[ mower.HP ]]</td>
-          <td>[[ mower.Engine ]]</td>
-          <td>[[ mower.Engine_Model ]]</td>
-          <td>[[ mower.Fuel ]]</td>
-          <td>[[ mower.MSRP ]]</td>
-          <td><input size="4" ng-model="mower.Sale" value="[[ mower.Sale ]]"</td>
-          <td>[[ mower.Event ]]</td>
-          <td>[[ mower.Finance ]]</td>
-          <td>[[ mower.Warranty ]]</td>
-        </tr>
-        </tbody>
-      </table>
-      <span class="btn btn-success" ng-click="printMode = 1">Print Quote</span>
-    </div>
-  </div>
-  <div ng-show="printMode">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 text-center">
-          <img src="http://mainstreetmower.com/logo.png" width="500px" />
-          <h4>Custom Quote</h4>
-        </div>
-        <hr/>
-        <p><strong>Note:</strong> <i>Good information for those receiving a quote can leave with</i>.</p>
-        <hr/>
-      </div>
-      <ul class="media-list">
-        <li class="media" ng-repeat="mower in filteredMowers">
-          <div class="media-left">
-            <a href="#">
-              <img width="90px" class="media-object" ng-src="[[ mower.Image ]]">
-            </a>
-          </div>
-          <div class="media-body">
-            <h4 class="media-heading">[[ mower.Brand ]] [[ mower.Series ]] - <small>[[ mower.Model ]]</small></h4>
-            <p><span class="lead">[[ mower.Sale | currency ]]</span> | [[ mower.HP ]]HP [[ mower.Engine ]] [[ mower.Engine_Model ]] | [[ mower.Width ]]" Cutting Width | [[ mower.Warranty ]] Warranty | Financing from [[ mower.Finance | currency ]]</p>
-          </div>
-        </li>
-      </ul>
-    </div>
+<body>
+<div class="container">
+  <div class="content">
+    <div class="title">Laravel 5</div>
+    <div class="quote">{{ Inspiring::quote() }}</div>
   </div>
 </div>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-<!-- AngularJS -->
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.11/angular.min.js"></script>
-<!-- AngularJS Filters -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/angular-filter/0.5.2/angular-filter.min.js"></script>
-<script src="/app.js"></script>
 </body>
 </html>
